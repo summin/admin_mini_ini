4c834aeb5c95e4eb54b927f134d418557b183e9a
Aleksandr Leontjev <alexander.leontjev@gmail.com>
2019-07-14 08:47:40 +0200

    dev

--- app/_devapp/Header.jsx
+++ app/_devapp/Header.jsx
@@ -13,6 +13,10 @@ import { render } from 'react-dom';
 const navStyle = {
     backgroundColor: '#e3f2fd',
 }
+const navInnerStyle = {
+    backgroundColor: 'white',
+
+}
 const navlogoStyle = {
     height: '30px',
 }
@@ -25,8 +29,8 @@ export default class Header extends Component {
             <Navbar expand="lg" className="header" style={navStyle}>
                 <Navbar.Brand href="#home"><b.Image src={navlogo} style={navlogoStyle}/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto justify-content-right">
+                <Navbar.Collapse id="basic-navbar-nav" style={navInnerStyle}>
+                    <Nav className="mr-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#link">Link</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
@@ -43,43 +47,6 @@ export default class Header extends Component {
                     </Form>
                 </Navbar.Collapse>
             </Navbar>
-
-
-            //<div className="container header">
-            //    <nav className="navbar navbar-expand-lg navbar-light" style={navStyle}>
-            //        <img src={logo} style={logoStyle} />
-            //        
-            //        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            //            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            //            <span className="navbar-toggler-icon"></span>
-            //        </button>
-            //        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            //            <ul className="nav ml-auto">
-            //                <li className="nav-item">
-            //                    <a className="nav-link" href="#">Config</a>
-            //                </li>
-            //                <li className="nav-item">
-            //                    <a className="nav-link" href="#">Days</a>
-            //                </li>
-            //                <li className="nav-item">
-            //                    <a className="nav-link" href="#">Images</a>
-            //                </li>
-            //                <li className="nav-item dropdown">
-            //                    <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-            //                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-            //                        Account 
-            //                    </a>
-            //                    <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-            //                        <a className="dropdown-item" href="#">Action</a>
-            //                        <a className="dropdown-item" href="#">Another action</a>
-            //                        <div className="dropdown-divider"></div>
-            //                        <a className="dropdown-item" href="#">Something else here</a>
-            //                    </div>
-            //                </li>
-            //            </ul>
-            //        </div>
-            //    </nav>
-            //</div>
         )
     }
 }
